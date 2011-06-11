b05d741c7707b5846f154a13606cea7d9f2d1974
Hajime Nakagami <nakagami@gmail.com>
2011-06-11 17:09:19 +0900

    add module path in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
 import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'planetplanet.settings'
+sys.path.insert(0, '/home/dotcloud/current/planetplanet')
 import django.core.handlers.wsgi
 djangoapplication = django.core.handlers.wsgi.WSGIHandler()
 def application(environ, start_response):
